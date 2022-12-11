aa799bd90a6da98e4b9b4d61618bdd01a5d75594
Berk Özler <brkozler@hotmail.com>
2022-12-11 22:32:14 +0200

    Fix notes in some commands

--- commands.go
+++ commands.go
@@ -337,7 +337,7 @@ func init() {
 **Halo Wars 2** : %s
 **Halo Infinite** : %s
 
-Note: If you finished everything and played any game on a non-XBL platform, please post your in-game screenshots now and ping a staff member.`
+Note: **If you finished everything and played any game on a non-XBL platform, please ping a staff member with screenshot proof in <#984079675385077820>.**`
 				failMsg = fmt.Sprintf(failMsg,
 					GetCompletionSymbol(completionMap["Halo: The Master Chief Collection"]),
 					GetCompletionSymbol(completionMap["Halo 5: Guardians"]),
@@ -466,8 +466,8 @@ Instead of **Halo MCC**, you can do:
 **Halo Reach** : %s
 **Halo 4** : %s
 
-Note: **If your SA/SS completion is not correct, those achievements might be bugged. Ping staff with screenshot proof in <#984079675385077820> if it blocks you from obtaining the role**
-Note: If you finished everything and played any game on a non-XBL platform, please post your in-game screenshots now and ping a staff member.`
+Note 1: **If your SA/SS completion is not correct, those achievements might be bugged. Ping a staff member with screenshot proof in <#984079675385077820> if it blocks you from obtaining the role**
+Note 2: **If you finished everything and played any game on a non-XBL platform, please ping a staff member with screenshot proof in <#984079675385077820>.**`
 				failMsg = fmt.Sprintf(failMsg,
 					GetCompletionSymbol(completionMap["Halo: The Master Chief Collection"]),
 					GetCompletionSymbol(completionMap["Halo Wars: Definitive Edition (PC)"]),
