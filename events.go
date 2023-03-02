f95a0624bd28bf6fc7f144a85760d28e27c26eac
brkzlr <brkozler@gmail.com>
2023-03-02 23:08:40 +0200

    Remove check for bot pinging Add check for discord server invites posts

--- events.go
+++ events.go
@@ -14,18 +14,22 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if m.Author.Bot {
 		return
 	}
-	// Explain users that ping the bot how to use the bot.
-	for _, user := range m.Mentions {
-		if user.ID == s.State.User.ID && m.MessageReference == nil { // Check for MessageReference since Mentions include replies.
-			s.MessageReactionAdd(m.ChannelID, m.ID, "🤡")
-			str := fmt.Sprintf("Don't ping me. Type `+help` in <#%s>", botChannelID)
-			ReplyToMsg(s, m.Message, str)
-			return
-		}
+
+	// Should probably move these blocks below into their own function
+	/////////////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////// Check for discord invites /////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////////////////////////////////
+	msg := strings.ToLower(m.Content)
+	if !IsStaff(m.Member) &&
+		(strings.Contains(msg, "discord.gg/") || strings.Contains(msg, "discord.com/invite/")) {
+		s.ChannelMessageDelete(m.ChannelID, m.ID)
+		return
 	}
 
+	/////////////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////// Check for proof-of-completion channel misuse //////////////////////////////
+	/////////////////////////////////////////////////////////////////////////////////////////////////////
 	if m.ChannelID == proofChannelID && !IsStaff(m.Member) {
-		msg := strings.ToLower(m.Content)
 		if (strings.Contains(msg, "mcc") && !strings.Contains(msg, "master")) ||
 			strings.Contains(msg, "chief collection") ||
 			strings.Contains(msg, "infinite") ||
@@ -59,10 +63,11 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 			}
 		}
 	}
+	/////////////////////////////////////////////////////////////////////////////////////////////////////
+
 	if !strings.HasPrefix(m.Content, "+") {
 		return
 	}
-
 	cmdContent := strings.Split(m.Content, " ")
 	cmdName := strings.ToLower(cmdContent[0])
 
