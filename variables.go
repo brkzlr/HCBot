658fec5d13853fb9595d909a4b6e8b522691c58b
brkzlr <brksys@icloud.com>
2023-06-08 19:23:10 +0300

    Add message combo breaker checks for fun

--- variables.go
+++ variables.go
@@ -21,18 +21,20 @@ const (
 	proofChannelID = "984079675385077820"
 )
 
+var currentComboMsgs = make(map[string]string)
+
 type TimedRoles struct {
 	ID   int
 	Game string
 }
 
 var timedAchievRoles = map[int]TimedRoles{
-	15: TimedRoles{990601630237982750, "Halo: Combat Evolved"},
-	9:  TimedRoles{990601760961875998, "Halo 2 Classic"},
-	25: TimedRoles{990601846391463967, "Halo 3"},
-	6:  TimedRoles{990602246888779837, "Halo 4"},
-	22: TimedRoles{990601924703297586, "Halo 3: ODST"},
-	14: TimedRoles{990602198184501268, "Halo: Reach"},
+	15: {990601630237982750, "Halo: Combat Evolved"},
+	9:  {990601760961875998, "Halo 2 Classic"},
+	25: {990601846391463967, "Halo 3"},
+	6:  {990602246888779837, "Halo 4"},
+	22: {990601924703297586, "Halo 3: ODST"},
+	14: {990602198184501268, "Halo: Reach"},
 }
 
 type RoleDate struct {
@@ -42,19 +44,19 @@ type RoleDate struct {
 
 var (
 	destinationVacationDates = []RoleDate{
-		RoleDate{1, time.January},
-		RoleDate{7, time.July},
-		RoleDate{31, time.October},
-		RoleDate{25, time.December},
+		{1, time.January},
+		{7, time.July},
+		{31, time.October},
+		{25, time.December},
 	}
 	elderSignsDates = []RoleDate{
-		RoleDate{1, time.January},
-		RoleDate{22, time.April},
-		RoleDate{5, time.May},
-		RoleDate{4, time.July},
-		RoleDate{7, time.July},
-		RoleDate{31, time.October},
-		RoleDate{11, time.November},
-		RoleDate{25, time.December},
+		{1, time.January},
+		{22, time.April},
+		{5, time.May},
+		{4, time.July},
+		{7, time.July},
+		{31, time.October},
+		{11, time.November},
+		{25, time.December},
 	}
 )
